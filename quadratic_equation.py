ca0afd373661039f39c29569385e13e54345bb55
DM45 <mda.tomsk@gmail.com>
2017-08-07 21:30:37 +0700

    Решение задачи по решению квадратного уравнения

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -3,9 +3,18 @@ from math import sqrt
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    root1 = (-b - sqrt(discriminant)) / (2 * a)
-    root2 = (-b + sqrt(discriminant)) / (2 * a)
-    if discriminant == 0:
-        return root1, None
+    if discriminant >= 0:
+        root1 = (-b - sqrt(discriminant)) / (2 * a)
+        root2 = (-b + sqrt(discriminant)) / (2 * a)
+        if discriminant == 0:
+            return root1, None
+        else:
+            return root1, root2
     else:
-        return root1, root2
+        return None, None
+
+
+if __name__ == '__main__':
+    starshiy_koef, sredniy_koef, svobodniy_ch =
+    map(int, input('Введите через пробел 3 коэффициента: ').split())
+    print(get_roots(starshiy_koef, sredniy_koef, svobodniy_ch))
